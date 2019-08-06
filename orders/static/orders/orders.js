95f349ca9242d2cab414350fee848126e45d8633
GiulianoSoria <giuliano.soria@gmail.com>
2019-08-06 11:49:49 -0700

    Complete regular pizza modal

--- orders/static/orders/orders.js
+++ orders/static/orders/orders.js
@@ -1,40 +1,86 @@
-document.addEventListener('DOMContentLoaded', function() {
-    $('.topping-select').hide();
-    $('#NumberOfToppings').on('change', function(){
-        if (this.value === 'ToppingSelection0') {
-            $('#ToppingSelection1').hide();
-            $('#ToppingSelection2').hide();
-            $('#ToppingSelection3').hide();
-            $('#ToppingSelection4').hide();
-            $('#ToppingSelection5').hide();
+document.addEventListener('DOMContentLoaded', () => {
+    $("#RegularPizzaModalButton1").on('click', function(e) {
+        e.preventDefault();
+        $('#RegularPizzaModal1').modal('show');
+    });
+    $("#RegularPizzaModalButton2").on('click', function(e) {
+        e.preventDefault();
+        $('#RegularPizzaModal2').modal('show');
+    });
+    $("#RegularPizzaModalButton3").on('click', function(e) {
+        e.preventDefault();
+        $('#RegularPizzaModal3').modal('show');
+    });
+    $("#RegularPizzaModalButton4").on('click', function(e) {
+        e.preventDefault();
+        $('#RegularPizzaModal4').modal('show');
+    });
+    $("#RegularPizzaModalButton5").on('click', function(e) {
+        e.preventDefault();
+        $('#RegularPizzaModal5').modal('show');
+    });
+
+    // $('#RegularPizzaPriceSmall').hide();
+    $('#RegularPizzaPriceLarge1').hide();
+    $('#RegularPizzaPriceLarge2').hide();
+    $('#RegularPizzaPriceLarge3').hide();
+    $('#RegularPizzaPriceLarge4').hide();
+    $('#RegularPizzaPriceLarge5').hide();
+
+    $('#RegularPizzaSizeSelection1').on('change', function() {
+        
+        if (this.value === 'S') {
+            $('#RegularPizzaPriceSmall1').show();
+            $('#RegularPizzaPriceLarge1').hide();
         }
-        else if (this.value === 'ToppingSelection1') {
-            $('#ToppingSelection1').show();
-            $('#ToppingSelection2').hide();
-            $('#ToppingSelection3').hide();
-            $('#ToppingSelection4').hide();
-            $('#ToppingSelection5').hide();
-        }
-        else if (this.value === 'ToppingSelection2') {
-            $('#ToppingSelection1').show();
-            $('#ToppingSelection2').show();
-            $('#ToppingSelection3').hide();
-            $('#ToppingSelection4').hide();
-            $('#ToppingSelection5').hide();
-        }
-        else if (this.value === 'ToppingSelection3') {
-            $('#ToppingSelection1').show();
-            $('#ToppingSelection2').show();
-            $('#ToppingSelection3').show();
-            $('#ToppingSelection4').hide();
-            $('#ToppingSelection5').hide();
-        }
-        else if (this.value === 'ToppingSelection4') {
-            $('#ToppingSelection1').show();
-            $('#ToppingSelection2').show();
-            $('#ToppingSelection3').show();
-            $('#ToppingSelection4').show();
-            $('#ToppingSelection5').show();
+        else if (this.value === 'L') {
+            $('#RegularPizzaPriceSmall1').hide();
+            $('#RegularPizzaPriceLarge1').show();
         }
     });
+    $('#RegularPizzaSizeSelection2').on('change', function() {
+        
+        if (this.value === 'S') {
+            $('#RegularPizzaPriceSmall2').show();
+            $('#RegularPizzaPriceLarge2').hide();
+        }
+        else if (this.value === 'L') {
+            $('#RegularPizzaPriceSmall2').hide();
+            $('#RegularPizzaPriceLarge2').show();
+        }
+    });
+    $('#RegularPizzaSizeSelection3').on('change', function() {
+        
+        if (this.value === 'S') {
+            $('#RegularPizzaPriceSmall3').show();
+            $('#RegularPizzaPriceLarge3').hide();
+        }
+        else if (this.value === 'L') {
+            $('#RegularPizzaPriceSmall3').hide();
+            $('#RegularPizzaPriceLarge3').show();
+        }
+    });
+    $('#RegularPizzaSizeSelection4').on('change', function() {
+        
+        if (this.value === 'S') {
+            $('#RegularPizzaPriceSmall4').show();
+            $('#RegularPizzaPriceLarge4').hide();
+        }
+        else if (this.value === 'L') {
+            $('#RegularPizzaPriceSmall4').hide();
+            $('#RegularPizzaPriceLarge4').show();
+        }
+    });
+    $('#RegularPizzaSizeSelection5').on('change', function() {
+        
+        if (this.value === 'S') {
+            $('#RegularPizzaPriceSmall5').show();
+            $('#RegularPizzaPriceLarge5').hide();
+        }
+        else if (this.value === 'L') {
+            $('#RegularPizzaPriceSmall5').hide();
+            $('#RegularPizzaPriceLarge5').show();
+        }
+    });
+
 });
